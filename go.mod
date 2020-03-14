5ed968e2716a6e753ab9ceae7d788a8d991de5aa
trad3r <tatusr@gmail.com>
2020-03-14 12:27:26 +0300

    add gin server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/trad3r/rest-api-go
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.5.0
--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", pong)
+
+	r.Run()
+}
+
+func pong(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
